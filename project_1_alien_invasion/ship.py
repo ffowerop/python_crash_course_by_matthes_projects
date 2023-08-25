c71534ce85b575035d1060502501ef522d7e30f4
ffowerop <sctp2k@gmail.com>
2023-08-25 11:07:16 +0300

    Add speed adjusment to the ship's movement

--- project_1_alien_invasion/ship.py
+++ project_1_alien_invasion/ship.py
@@ -6,12 +6,15 @@ class Ship():
 		"""Initializes a ship and sets it's initial position"""
 		self.screen = ai_game.screen
 		self.screen_rect = ai_game.screen.get_rect()
+		self.settings = ai_game.settings
 
 		#Loads ship's image and gets it's rectangle
 		self.image = pygame.image.load('images/ship.bmp')
 		self.rect = self.image.get_rect()
 		#Every new ship appears at the middle of bottom of the screen
 		self.rect.midbottom = self.screen_rect.midbottom
+		#Saving float coordinate of the ship's center
+		self.x = float(self.rect.x)
 		#Movememnt indicator
 		self.moving_left = False
 		self.moving_right = False
@@ -23,6 +26,8 @@ class Ship():
 	def update(self):
 		"""Update ship's position depending of the indicator"""
 		if self.moving_left:
-			self.rect.x -=1
+			self.x -= self.settings.ship_speed
 		if self.moving_right:
-			self.rect.x +=1
+			self.x += self.settings.ship_speed
+		#Updates ship's center position on the ground of self.x
+		self.rect.x = self.x
