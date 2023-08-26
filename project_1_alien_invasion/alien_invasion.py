ea8fa4377c64f4f7e6ab2e21d0ab26c6a47c21d0
ffowerop <sctp2k@gmail.com>
2023-08-26 15:30:48 +0300

    Refactoring self._update_bullets()

--- project_1_alien_invasion/alien_invasion.py
+++ project_1_alien_invasion/alien_invasion.py
@@ -47,6 +47,9 @@ class AlienInvasion:
 		for bullet in self.bullets.copy():
 			if bullet.rect.bottom <= 0:
 				self.bullets.remove(bullet)
+		self._check_bullet_alien_collision()
+
+	def _check_bullet_alien_collision(self):
 		collisions = pygame.sprite.groupcollide(
 			self.bullets, self.aliens, True, True)
 		#Destroys all bullets and creates new alien fleet
